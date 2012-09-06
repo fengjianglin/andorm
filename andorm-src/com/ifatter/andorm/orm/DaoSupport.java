a598959add34a508a659f2b0b9655400409861d4
iFatter <ifatter@gmail.com>
2012-09-06 16:51:16 +0800

    release andorm_base1.6_1.0.0.jar

--- andorm-src/com/ifatter/andorm/orm/DaoSupport.java
+++ andorm-src/com/ifatter/andorm/orm/DaoSupport.java
@@ -1,3 +1,18 @@
+/* 
+ * Copyright (C) 2011-2012 ifatter.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.ifatter.andorm.orm;
 
@@ -35,8 +50,8 @@ public abstract class DaoSupport<T> {
             try {
                 Constructor<? extends DBSupport> con = c.getConstructor();
                 DBSupport support = con.newInstance();
-                mDBHelper = new ORMSQLiteHelper(context, support.getName(),
-                        support.getVersion(), support.getClasses());
+                mDBHelper = new ORMSQLiteHelper(context, support.getName(), support.getVersion(),
+                        support.getClasses());
                 mDBHelper.setDabaseListener(mListener);
             } catch (Exception e) {
                 e.printStackTrace();
