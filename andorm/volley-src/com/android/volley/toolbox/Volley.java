c9ced44b5aa5510ef0f664c66c7d0c0f98573cf6
feng <fandyvon@163.com>
2016-10-13 15:27:31 +0800

    unknown

--- andorm/volley-src/com/android/volley/toolbox/Volley.java
+++ andorm/volley-src/com/android/volley/toolbox/Volley.java
@@ -29,86 +29,42 @@ import java.io.File;
 
 public class Volley {
 
-    /** Default on-disk cache directory. */
-    private static final String DEFAULT_CACHE_DIR = "volley";
+	private static final String DEFAULT_CACHE_DIR = "volley";
 
-    /**
-     * Creates a default instance of the worker pool and calls {@link RequestQueue#start()} on it.
-     *
-     * @param context A {@link Context} to use for creating the cache dir.
-     * @param stack An {@link HttpStack} to use for the network, or null for default.
-     * @return A started {@link RequestQueue} instance.
-     */
-    public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
-        File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
+	public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
+		File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
 
-        String userAgent = "volley/0";
-        try {
-            String packageName = context.getPackageName();
-            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, 0);
-            userAgent = packageName + "/" + info.versionCode;
-        } catch (NameNotFoundException e) {
-        }
+		String userAgent = "volley/0";
+		try {
+			String packageName = context.getPackageName();
+			PackageInfo info = context.getPackageManager().getPackageInfo(
+					packageName, 0);
+			userAgent = packageName + "/" + info.versionCode;
+		} catch (NameNotFoundException e) {
+		}
 
-        if (stack == null) {
-            if (Build.VERSION.SDK_INT >= 9) {
-                stack = new HurlStack();
-            } else {
-                // Prior to Gingerbread, HttpUrlConnection was unreliable.
-                // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-                stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
-            }
-        }
+		if (stack == null) {
+			if (Build.VERSION.SDK_INT >= 9) {
+				stack = new HurlStack();
+			} else {
+				// Prior to Gingerbread, HttpUrlConnection was unreliable.
+				// See:
+				// http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+				stack = new HttpClientStack(
+						AndroidHttpClient.newInstance(userAgent));
+			}
+		}
 
-        Network network = new BasicNetwork(stack);
+		Network network = new BasicNetwork(stack);
 
-        RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir), network);
-        queue.start();
+		RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir),
+				network);
+		queue.start();
 
-        return queue;
-    }
-    
-    /**
-     * context
-     * dir sdcard's root dire
-     * null
-     * */
-    public static RequestQueue newRequestQueueInDisk(Context context, String dir, HttpStack stack) {
-        File cacheDir = new File(dir, DEFAULT_CACHE_DIR);
+		return queue;
+	}
 
-        String userAgent = "volley/0";
-        try {
-            String packageName = context.getPackageName();
-            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, 0);
-            userAgent = packageName + "/" + info.versionCode;
-        } catch (NameNotFoundException e) {
-        }
-
-        if (stack == null) {
-            if (Build.VERSION.SDK_INT >= 9) {
-                stack = new HurlStack();
-            } else {
-                // Prior to Gingerbread, HttpUrlConnection was unreliable.
-                // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-                stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
-            }
-        }
-
-        Network network = new BasicNetwork(stack);
-
-        RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir), network);
-        queue.start();
-
-        return queue;
-    }
-
-    /**
-     * Creates a default instance of the worker pool and calls {@link RequestQueue#start()} on it.
-     *
-     * @param context A {@link Context} to use for creating the cache dir.
-     * @return A started {@link RequestQueue} instance.
-     */
-    public static RequestQueue newRequestQueue(Context context) {
-        return newRequestQueue(context, null);
-    }
+	public static RequestQueue newRequestQueue(Context context) {
+		return newRequestQueue(context, null);
+	}
 }
